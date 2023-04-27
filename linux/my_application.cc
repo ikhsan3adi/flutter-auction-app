9bd3e7ec18bb5c1e5feb56669155bc888c51490c
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-04-27 08:38:54 +0700

    fix: change app name

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "aplikasi_lelang_online");
+    gtk_header_bar_set_title(header_bar, "online_auction_app");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "aplikasi_lelang_online");
+    gtk_window_set_title(window, "online_auction_app");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"aplikasi_lelang_online", origin, size)) {
+  if (!window.CreateAndShow(L"online_auction_app", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
