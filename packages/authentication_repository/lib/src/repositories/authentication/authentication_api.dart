b0f0ef7fab47c3d7a8126fa7b11134f9486b194a
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-04-28 09:32:08 +0700

    Update authentication_api.dart

--- packages/authentication_repository/lib/src/repositories/authentication/authentication_api.dart
+++ packages/authentication_repository/lib/src/repositories/authentication/authentication_api.dart
@@ -2,56 +2,52 @@ import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 import 'package:user_repository/user_repository.dart';
 
-abstract class AuthenticationApiClient extends Equatable {}
+abstract class AuthenticationApiClient extends Equatable {
+  Future<void> register(User user);
+  Future<String> login(String username, String password);
+  Future<void> logout();
+}
 
 class AuthenticationApiClientImpl extends AuthenticationApiClient {
   AuthenticationApiClientImpl({required Dio dio}) : _dio = dio;
 
   final Dio _dio;
 
+  final String _secretKey =
+      'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY1MTAyOTU5MSwiaWF0IjoxNjUxMDI5NTkxfQ.GEqEGG2K-AQwjGEwDRzChXRIQVm5GtJPbTfcXHrgX5w';
+
   @override
   List<Object?> get props => [_dio];
 
-  Future<User> register(User user) async {
-    final response = await _dio.post(
-      '',
-      options: Options(
-        headers: {'Authorization': ''},
-      ),
+  @override
+  Future<void> register(User user) async {
+    await _dio.post(
+      '/user',
       data: user.toJson(),
+      options: Options(
+        headers: {'Authorization': 'Bearer $_secretKey'},
+      ),
     );
-    if (response.statusCode == 201) {
-      final json = response.data;
-      final createdUser = User.fromJson(json);
-      return createdUser;
-    } else {
-      throw Exception('Failed to register user');
-    }
   }
 
+  @override
   Future<String> login(String username, String password) async {
     final response = await _dio.post(
-      '',
+      '/login',
       data: {
         'username': username,
         'password': password,
       },
     );
-    if (response.statusCode == 200) {
-      final json = response.data;
-      final token = json['token'] as String;
-      return token;
-    } else {
-      throw Exception('Failed to login user');
-    }
+
+    final token = response.data['token'] as String;
+    return token;
   }
 
+  @override
   Future<void> logout() async {
-    final response = await _dio.post(
-      '',
+    await _dio.post(
+      '/logout',
     );
-    if (response.statusCode != 204) {
-      throw Exception('Failed to logout user');
-    }
   }
 }
