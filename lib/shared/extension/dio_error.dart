2b3b7633a08ac97156f88c5eab4c7493c9e634cb
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-04-30 06:20:04 +0700

    fix: fix null dio error messages

--- lib/shared/extension/dio_error.dart
+++ lib/shared/extension/dio_error.dart
@@ -2,11 +2,12 @@ import 'package:dio/dio.dart';
 
 extension DioErrorX on DioError {
   List<String> get errorsMessages {
-    if (requestOptions.data['messages'] != null) {
-      final List<String> errors = (requestOptions.data['messages'] as Map<String, String>).values.toList();
-
-      return errors;
+    if (requestOptions.data == null || requestOptions.data['messages'] == null) {
+      return [toString()];
     }
-    return [toString()];
+
+    final List<String> errors = (requestOptions.data['messages'] as Map<String, String>).values.toList();
+
+    return errors;
   }
 }
