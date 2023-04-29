5c9f575beb8220808e8e0d981c3d094670a28de1
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-04-30 06:17:12 +0700

    fix: init storage & dotenv
    Forgotten

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:user_repository/user_repository.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -8,10 +12,16 @@ import 'bootstrap.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb ? HydratedStorage.webStorageDirectory : await getApplicationDocumentsDirectory(),
+  );
+
   await Hive.initFlutter();
 
   Hive.registerAdapter(TokenAdapter());
   Hive.registerAdapter(UserAdapter());
+
+  await dotenv.load(fileName: ".env");
 
   bootstrap();
 }
