94d4e60b3e053248b1bf53b044921c5bbacd8f62
Ikhsan S <93766033+ikhsan3adi@users.noreply.github.com>
2023-05-23 13:22:16 +0700

    style: new color palette
    * bid price bg color

--- lib/shared/theme/color_palettes.dart
+++ lib/shared/theme/color_palettes.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ColorPalettes {
+  static Color highestBidBg = Colors.greenAccent;
+  static Color bidBg = Colors.amber;
+  static Color errBidBg = Colors.red[300]!;
+
   static Color shimmerBg = Colors.grey[500]!;
   static Color shimmer = Colors.grey[400]!;
 }
