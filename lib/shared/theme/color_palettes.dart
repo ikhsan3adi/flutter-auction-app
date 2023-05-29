464ab7c3af9c3fcf0b3179e9034915e353743e68
Ikhsan S <93766033+ikhsan3adi@users.noreply.github.com>
2023-05-29 21:10:27 +0700

    Update color_palettes.dart

--- lib/shared/theme/color_palettes.dart
+++ lib/shared/theme/color_palettes.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ColorPalettes {
-  static Color highestBidBg = Colors.greenAccent;
-  static Color bidBg = Colors.amber;
-  static Color errBidBg = Colors.red[300]!;
+  static Color greenHighlight = Colors.greenAccent;
+  static Color amberHighlight = Colors.amber;
+  static Color redHighlight = Colors.red[300]!;
+
+  static Color highlightedText = Colors.blue.shade900;
 
   static Color shimmerBg = Colors.grey[500]!;
   static Color shimmer = Colors.grey[400]!;
