b3d080170f5895891382b0ba585d459089594fa7
Ikhsan S <93766033+ikhsan3adi@users.noreply.github.com>
2023-06-03 19:35:12 +0700

    style(detail): fix error UI

--- lib/features/auction_detail/widgets/product_property.dart
+++ lib/features/auction_detail/widgets/product_property.dart
@@ -16,63 +16,58 @@ class AuctionProductProperty extends StatelessWidget {
     TextTheme textTheme = theme.textTheme;
     return Theme(
       data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-      child: ExpansionTile(
-        expandedAlignment: Alignment.topLeft,
-        tilePadding: const EdgeInsets.fromLTRB(16, 20, 16, 0),
-        initiallyExpanded: true,
-        title: BlocBuilder<AuctionDetailBloc, AuctionDetailState>(
-          builder: (context, state) {
+      child: BlocBuilder<AuctionDetailBloc, AuctionDetailState>(
+        builder: (context, state) {
+          if (state is! AuctionDetailError) {
+            DateTime? dateCreated;
+            DateTime? dateCompleted;
+
             if (state is AuctionDetailLoaded) {
-              return Text(state.auction.itemName, style: textTheme.headlineMedium);
-            } else if (state is AuctionDetailLoading) {
-              return Text(auction.itemName, style: textTheme.headlineMedium);
+              dateCreated = state.auction.createdAt;
+              dateCompleted = state.auction.dateCompleted;
             }
 
-            return Text(
-              "Error occurred",
-              style: textTheme.headlineMedium?.copyWith(color: theme.colorScheme.error),
+            return ExpansionTile(
+              expandedAlignment: Alignment.topLeft,
+              tilePadding: const EdgeInsets.fromLTRB(16, 20, 16, 0),
+              initiallyExpanded: true,
+              title: state is AuctionDetailLoaded
+                  ? Text(state.auction.itemName, style: textTheme.headlineMedium)
+                  : Text(auction.itemName, style: textTheme.headlineMedium),
+              children: [
+                BlocBuilder<AuctionDetailBloc, AuctionDetailState>(
+                  builder: (context, state) {
+                    return state is AuctionDetailLoaded
+                        ? Padding(
+                            padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text("Description: \n${state.auction.description}", style: textTheme.bodyMedium),
+                                const SizedBox(height: 20),
+                                Text(
+                                  "Date created: ${DateFormat("dd MMMM yyyy").format(dateCreated!)}",
+                                  style: textTheme.bodyMedium,
+                                ),
+                                Text(
+                                  "Date completed: ${dateCompleted != null ? DateFormat("dd MMMM yyyy").format(dateCompleted) : '-'}",
+                                  style: textTheme.bodyMedium,
+                                ),
+                              ],
+                            ),
+                          )
+                        : const Padding(
+                            padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+                            child: ShimmerItemDescription(),
+                          );
+                  },
+                ),
+              ],
             );
-          },
-        ),
-        children: [
-          BlocBuilder<AuctionDetailBloc, AuctionDetailState>(
-            builder: (context, state) {
-              TextTheme textTheme = Theme.of(context).textTheme;
-              if (state is AuctionDetailLoaded) {
-                DateTime dateCreated = state.auction.createdAt;
-                DateTime? dateCompleted = state.auction.dateCompleted;
-                return Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text("Description: \n${state.auction.description}", style: textTheme.bodyMedium),
-                      const SizedBox(height: 20),
-                      Text(
-                        "Date created: ${DateFormat("dd MMMM yyyy").format(dateCreated)}",
-                        style: textTheme.bodyMedium,
-                      ),
-                      Text(
-                        "Date completed: ${dateCompleted != null ? DateFormat("dd MMMM yyyy").format(dateCompleted) : '-'}",
-                        style: textTheme.bodyMedium,
-                      ),
-                    ],
-                  ),
-                );
-              } else if (state is AuctionDetailLoading) {
-                return const Padding(
-                  padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
-                  child: ShimmerItemDescription(),
-                );
-              }
-
-              return Text(
-                "Error occurred",
-                style: textTheme.headlineMedium?.copyWith(color: Colors.red),
-              );
-            },
-          ),
-        ],
+          } else {
+            return ErrorCommon(message: state.messages[0]);
+          }
+        },
       ),
     );
   }
