3a921fbb8d444c6847ec216f2db6f9b13febdacb
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-06-28 23:08:09 +0700

    style: add basic shimmer widget

--- lib/shared/widgets/shimmer/shimmer.dart
+++ lib/shared/widgets/shimmer/shimmer.dart
@@ -3,3 +3,4 @@ export 'shimmer_product_card.dart';
 export 'shimmer_product_list_tile.dart';
 export 'shimmer_detail_carousel.dart';
 export 'shimmer_item_description.dart';
+export 'shimmer_basic.dart';
---
+++ lib/shared/widgets/shimmer/shimmer_basic.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_online_auction_app/shared/theme/theme.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerBasic extends StatelessWidget {
+  const ShimmerBasic({super.key, this.child});
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: ColorPalettes.shimmerBg,
+      highlightColor: ColorPalettes.shimmer,
+      child: Container(
+        color: ColorPalettes.shimmerBg,
+        child: child,
+      ),
+    );
+  }
+}
