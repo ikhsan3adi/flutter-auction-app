e204fe03d873e1e255fc54ad245e2fb9edc3fe04
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-07-02 17:26:14 +0700

    feat: logout button added

--- lib/features/home/widgets/drawer.dart
+++ lib/features/home/widgets/drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_online_auction_app/features/auth/auth.dart';
 import 'package:flutter_online_auction_app/shared/shared.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class MyCustomDrawer extends StatelessWidget {
   const MyCustomDrawer({super.key});
@@ -28,9 +30,19 @@ class MyCustomDrawer extends StatelessWidget {
                 ),
               ),
               ListTile(
-                leading: const Icon(Icons.settings),
-                title: const Text("Settings"),
-                onTap: () {},
+                leading: const Icon(Icons.logout),
+                title: const Text("Logout"),
+                onTap: () async {
+                  await showDialog<bool>(
+                    context: context,
+                    builder: (context) => const ConfirmDialog(),
+                  ).then((logout) {
+                    if (logout ?? false) {
+                      Fluttertoast.showToast(msg: 'Processing...');
+                      context.read<AuthBloc>().add(AuthLogoutRequestedEvent());
+                    }
+                  });
+                },
               ),
               const Divider(),
               Column(
