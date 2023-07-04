6b5dd7d41c2b55761b0970ef8de8def603b722c1
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-07-04 10:22:04 +0700

    Update splash_logo.dart
    fix black logo in dark mode

--- lib/features/splash/widgets/splash_logo.dart
+++ lib/features/splash/widgets/splash_logo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_online_auction_app/shared/shared.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashLogo extends StatelessWidget {
@@ -12,10 +14,14 @@ class SplashLogo extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        SvgPicture.asset(
-          'assets/logo/logo.svg',
-          theme: SvgTheme(currentColor: textTheme.headlineLarge?.color ?? Colors.black87),
-          height: MediaQuery.of(context).size.height / 5,
+        BlocBuilder<AppThemeCubit, AppThemeState>(
+          builder: (context, state) {
+            return SvgPicture.asset(
+              'assets/logo/logo.svg',
+              theme: SvgTheme(currentColor: state.themeMode == ThemeMode.dark ? Colors.white : Colors.black87),
+              height: MediaQuery.of(context).size.height / 5,
+            );
+          },
         ),
         Text("E-AUCTION", style: textTheme.headlineLarge),
         const Text('Online Auction App'),
